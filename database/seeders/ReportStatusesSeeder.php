fdb2560f5882ba8ccaabc29d528090dfd00221ab
SenszZ00 <jlurzano13@gmail.com>
2025-09-11 23:44:36 +0800

    added predefined statuses

---
+++ database/seeders/ReportStatusesSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ReportStatusesSeeder extends Seeder
+{
+    public function run(): void
+    {
+        DB::table('report_statuses')->insert([
+            ['name' => 'pending'],
+            ['name' => 'under review'],
+            ['name' => 'resolved'],
+        ]);
+    }
+}
